7f14f775c09896e1721b0363acef98f2b9485b48
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-02-16 01:04:09 +0000

    updates made

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,18 +1,37 @@
-class ContactsController < ApplicationController
+class CompaniesController < ApplicationController
 
-    def show
-      @place = Place.find(params["id"])
-    end
-  
-    def new
-      @place = Place.new
-      end
-  
-    def create
-      @place = Place.new(params["place"])
-      @place.save
-      redirect_to "/posts/#{@posts.place.id}"
-    end
-  
+  def index
+    @places = Place.all
   end
-  
+
+  def show
+    @place = Place.find(params["id"])
+  end
+
+  def new
+    @place = Company.new
+  end
+
+  def create
+    @place = Place.new(params["place"])
+    @place.save
+    redirect_to "/places"
+  end
+
+  def edit
+    @place = Place.find(params["id"])
+  end
+
+  def update
+    @place = Place.find(params["id"])
+    @place.update(params["place"])
+    redirect_to "/places"
+  end
+
+  def destroy
+    @place = Company.find(params["id"])
+    @place.destroy
+    redirect_to "/places"
+  end
+
+end
