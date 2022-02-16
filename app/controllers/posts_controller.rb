3a9bf30d53b45693d831f5b020c9601fceef0d32
JacobGleas0n <jacob.gleason@kellogg.northwestern.edu>
2022-02-16 01:49:53 +0000

    some progress made

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,13 @@ class PostsController < ApplicationController
   
     def new
       @post = Post.new
-      @place = Place.find(params["place_id"])
-      @post.place_id = @place.id
-    
+      @post.place_id = params["place_id"]
     end
   
     def create
       @post = Post.new(params["place"])
       @post.save
-      redirect_to "/places/#{@post.place.id}"
+      redirect_to "/places/#{@post.place_id}"
     end
   
   end
